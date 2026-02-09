9e1c4692ca177d28819631c7a70235aabb89907b
Luis Silva <luissilvam537@gmail.com>
2026-02-09 18:04:15 -0300

    Implementing the Menu and PreStart

--- Program.cs
+++ Program.cs
@@ -7,12 +7,47 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("===== Welcome to StopWatch! ===== ");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 10m = 10 minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+            
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!");
+            Thread.Sleep(2500);
+            
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -22,6 +57,9 @@ namespace StopWatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.WriteLine("Stop Watch concluído, obrigado.");
+            Thread.Sleep(3000);
+            Menu();
         }
     }
 }
